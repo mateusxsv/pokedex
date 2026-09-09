643d7b63b0f305d0f8f5398c270349378e9fd73f
mateusxsv <mateus.araujo.banana@gmail.com>
2026-09-09 11:07:30 -0300

    segundo commit - correcoes

---
+++ script.js
@@ -0,0 +1,57 @@
+
+const API = "https://pokeapi.co/api/v2/pokemon/";
+
+async function carregarPokemon(numero) {
+
+    const resposta = await fetch(API + numero);
+    const pokemon = await resposta.json();
+
+    return pokemon;
+ }
+
+
+async function carregarPokemons() {
+
+    const grid = document.querySelector("#pokemons");
+
+    grid.innerHTML = "Carregando Pokémon...";
+
+    const pokemons = [];
+
+    for (let i = 0; i < 20; i++) {
+
+        const numero = Math.floor(Math.random() * 1025) + 1;
+
+        const pokemon = await carregarPokemon(numero);
+
+        pokemons.push(pokemon);
+    }
+
+    grid.innerHTML = "";
+
+    pokemons.forEach(pokemon => {
+
+        const tipos = pokemon.types
+            .map(tipo => tipo.type.name)
+            .join(" / ");
+
+        grid.innerHTML += `
+            <article class="pokemon">
+
+                <img
+                    src="${pokemon.sprites.front_default}"
+                    alt="${pokemon.name}"
+                >
+
+                <h3>${pokemon.name}</h3>
+
+                <p>#${pokemon.id}</p>
+
+                <p>Tipo: ${tipos}</p>
+
+            </article>
+        `;
+    });
+}
+
+carregarPokemons();
